0db40f45c2da7739d47e8028255422a8653bcc22
“nj-lym <email@vip_lym@163.comgit config --global user.name nj-lymgit config --global user.email email@vip_lym@163.com>
2020-07-22 18:30:52 +0800

    创建项目lym

---
+++ src/test/java/com/lym/myblog/MyblogApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lym.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
